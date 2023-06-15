93a324bf9a5dad2608b23b2dc9789126d3c250fd
brooke <bhilbran@asu.edu>
2023-06-14 18:12:56 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat (name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+function appendCat(name){
+    let newArray = cats.slice(name)
+    newArray.push(name)
+    return newArray
+}
+function prependCat(name){
+    let newArray = cats.slice(name)
+    newArray.unshift(name)
+    return newArray
+}
+function removeLastCat(name){ 
+let newArray = cats.slice(name)
+    newArray.pop(name)
+    return newArray
+}       
+function removeFirstCat(name){ 
+    let newArray = cats.slice(name)
+        newArray.shift(name)
+        return newArray
+    }       
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,7 @@
 require ( './helpers.js' );
 
-describe('index.js', function () {
-  describe('cats', function () {
+describe('index.js', function (n) {
+  describe('cats', function (n) {
     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
       expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
     });
